e1fe1946a16f69f332c19750972d6fd73bd11a4a
Nasim Ul Haq <qadart@gmail.com>
2016-10-16 06:55:59 +0500

    Add project files.

---
+++ ApprovalSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApprovalSystem.Startup))]
+namespace ApprovalSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
